8b49abb6f8b40c36daea860448ae4c792668752b
Helvécio Tedesco <helvecio@teccard.com.br>
2022-09-27 09:59:09 -0300

    Adiciona arquivos

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -0,0 +1,44 @@
+package br.com.alura.mvc.mudi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+	
+	@Autowired // UTILIZADO PARA INJEÇÃO DE DEPENDÊNCIA - O SPRING INSTANCIA ESSA CLASSE AUTOMATICAMENTE
+	private PedidoRepository pedidoRepository;
+
+	@GetMapping
+	public String home(Model model) {	
+		List<Pedido> pedidos = pedidoRepository.findAll();
+		model.addAttribute("pedidos", pedidos); //PASSANDO A LISTA PEDIDOS NA REQUISIÇÃO POR PARÂMETRO
+		return "home";
+	}
+	
+	@GetMapping("/{status}")
+	public String porStatus(@PathVariable("status")String status, Model model) {	
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+		model.addAttribute("pedidos", pedidos); //PASSANDO A LISTA PEDIDOS NA REQUISIÇÃO POR PARÂMETRO
+		model.addAttribute("status", status);
+		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
+	}
+	
+}
---
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,20 @@
+package br.com.alura.mvc.mudi.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+	List<Pedido> findByStatus(StatusPedido status);
+	//findAll já está na JpaRepository. Veio para cá por meio do extends.
+}
